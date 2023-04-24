d803e691ddb78a58f56d3e020bb456d0c519e7c2
olvzolvz <125333402+olvzolvz@users.noreply.github.com>
2023-04-24 17:56:39 +0700

    add about

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 function toggleDetail(e) {
-    const target=$(e.target)
+    const target= $(e.target)
+
+    if($(target).hasClass("active")){
+        $(target).html("More Info").removeClass("active")
+    } else {
+        $(target).html("Less Info").addClass("active")
+    }
+
     
-    console.log($(target).parents(".about-exp-item"))
+    const item = $(target).parents(".about-exp-item")
     const detail = $(item).children(".about-exp-item-detail")
 
     $(detail).slideToggle()
-   
-    console.log($(item).children(".about-exp-item-detail"))
 
-}
+    }
